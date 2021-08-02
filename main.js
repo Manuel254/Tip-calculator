42448f4988ae269dacecc981f8d2547002cb05e1
Manuel254 <Immanuelkituku@gmail.com>
2021-08-02 21:59:57 +0300

    design

---
+++ main.js
@@ -0,0 +1,17 @@
+// const billInput = document.getElementById("bill-input");
+// const peopleInput = document.getElementById("people-input");
+// const btn5 = document.getElementById("btn-5");
+// const btn10 = document.getElementById("btn-10");
+// const btn15 = document.getElementById("btn-15");
+// const btn25 = document.getElementById("btn-25");
+// const btn50 = document.getElementById("btn-50");
+// const custom = document.getElementById("custom");
+// const tip = document.querySelector(".tip-amount");
+// const total = document.querySelector(".total-amount");
+// const resetBtn = document.querySelector(".reset-btn");
+
+// const bill = () => {
+//   return (tip.innerText = billInput.value);
+// };
+
+// bill();
